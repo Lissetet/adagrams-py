f1f5b7ec8868466f2230103646796ecad3b88730
liz <lissete.trejo@gmail.com>
2023-03-16 15:01:21 -0700

    finished wave 2 and 3, passed tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -14,13 +14,28 @@ def draw_letters():
 	return random.sample(available_pool, 10)
 
 def uses_available_letters(word, letter_bank):
-	pass
+	letter_bank_copy = list(letter_bank)
+	for letter in word.upper():
+		if letter not in letter_bank_copy:
+			return False
+		letter_bank_copy.remove(letter)
+	return True
+
 
 def score_word(word):
-	pass
+	total = 0
+	letter_scores = {
+		'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1,
+		'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1,
+		'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10
+	}
+	for letter in word:
+		total += letter_scores[letter.upper()]
+
+	if len(word) >= 7:
+		total += 8
+	
+	return total
 
 def get_highest_word_score(word_list):
 	pass
-
-
-draw_letters()
